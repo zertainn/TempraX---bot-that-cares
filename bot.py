9883c395e99c996cde9ec4038643928c1ca3b11f
zertainn <avramenkom090@gmail.com>
2025-04-23 13:38:10 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+TOKEN = "7574040177:AAECd04LZ-aCDSHTYGf-DAlYKPfTd6fquA4"
+
+bot = telebot.TeleBot(TOKEN)
+
+def make_keyboard():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard = True)
+    keyboard.add(KeyboardButton('/start'))
+    return keyboard
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Приветствую')
+    bot.send_message(message.chat.id, 'Используй команду /help, чтобы узнать о всех доступных командах', reply_markup=make_keyboard())
+
+bot.infinity_polling()
